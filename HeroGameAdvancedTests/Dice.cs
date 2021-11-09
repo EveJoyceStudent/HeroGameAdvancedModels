19accac62ba5999c4a3af2cb1dfb3fcf1a495585
jellyve <a.eve.joyce@gmail.com>
2021-11-09 11:11:51 +1100

    added unit tests

--- HeroGameAdvancedTests/Dice.cs
+++ HeroGameAdvancedTests/Dice.cs
@@ -6,10 +6,38 @@ namespace HeroGameAdvancedTests
 {
     public class DiceTests
     {
-        [Fact]
-        public void Test1()
-        {
+        Dice dice0;
+        Dice dice1;
+        Dice dice2;
+        Dice dice3;
+        Dice dice4;
+        Dice dice5;
+        Dice[] diceList;
 
+        public DiceTests(){
+            this.dice0 = new Dice(1,6);
+            this.dice1 = new Dice(1,1);
+            this.dice2 = new Dice(0,5);
+            this.dice3 = new Dice(-5,5);
+            this.dice4 = new Dice(-5,0);
+            this.dice5 = new Dice(-1,-1);
+            this.diceList = new Dice[]{ this.dice0, this.dice1, this.dice2, this.dice3, this.dice4, this.dice5 };
+        }
+
+        [Theory]
+        [InlineData(0,1,6)]
+        [InlineData(1,1,1)]
+        [InlineData(2,0,5)]
+        [InlineData(3,-5,5)]
+        [InlineData(4,-5,0)]
+        [InlineData(5,-1,-1)]
+        public void DiceValueTests(int diceIndex, int diceValueMin, int diceValueMax)
+        {
+            int diceValue;
+            for(int i=0; i<100; i++){
+                diceValue = this.diceList[diceIndex].Roll();
+                Assert.InRange(diceValue, diceValueMin, diceValueMax);
+            }
         }
     }
 }
--- HeroGameAdvancedTests/VillainTests.cs
+++ HeroGameAdvancedTests/VillainTests.cs
@@ -6,10 +6,36 @@ namespace HeroGameAdvancedTests
 {
     public class VillainTests
     {
-        [Fact]
-        public void Test1()
-        {
+        Villain villainTest1;
+        Villain villainTest2;
 
+        public VillainTests(){
+            this.villainTest1 = new Villain(1,"Test1",3);
+            this.villainTest2 = new Villain(2,"Test2",0);
+        }
+
+        [Theory]
+        [InlineData(1,2)]
+        [InlineData(0,3)]
+        [InlineData(3,0)]
+        [InlineData(4,0)]
+        [InlineData(-1,4)]
+        public void Villain1AttackTests(int damageValue, int expectedHealth)
+        {
+            this.villainTest1.Damage(damageValue);
+            Assert.Equal(expectedHealth, this.villainTest1.Hitpoints);
+        }
+
+        [Theory]
+        [InlineData(1,0)]
+        [InlineData(0,0)]
+        [InlineData(3,0)]
+        [InlineData(4,0)]
+        [InlineData(-1,1)]
+        public void Villain2AttackTests(int damageValue, int expectedHealth)
+        {
+            this.villainTest2.Damage(damageValue);
+            Assert.Equal(expectedHealth, this.villainTest1.Hitpoints);
         }
     }
 }
